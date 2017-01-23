40f7fe57588db6f9cf68b5e22eb003bf1cb78a37
unknown <17229398@qq.com>
2017-01-23 19:44:32 +0800

    Signed-off-by: unknown <17229398@qq.com>

---
+++ js/common.js
@@ -0,0 +1,30 @@
+jQuery(function($){
+	var fnbook_i = $("#m-sidebar-list li"),
+		fnbook_c = $("#m-content-list .m-content-list-item");
+
+	fnbook_i.each(function(){
+		if($(this).children('a').length == 0){
+			$(this)
+			.wrapInner('<a></a>')
+			.find('a')
+			.attr('href',function(){return "#"+$(this).text()})
+			.attr('target','_self')
+		}
+	})
+	fnbook_c
+		.attr('id',function(){
+			return $(this).find('h3').children('b').text()
+		})
+	fnbook_i.on('click',function(){
+		//自身样式
+		fnbook_i.removeClass('current')
+		$(this).addClass('current')
+		//目标样式
+		var index = $(this).index('#m-sidebar-list li')
+		fnbook_c.eq(index).addClass('active')
+		var t = setTimeout(function(){
+			fnbook_c.eq(index).removeClass('active')
+			clearTimeout(t)
+		},1000)
+	})
+});
